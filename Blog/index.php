028f0d89b908b598f891cafebadc89bc7c441203
Audric Larose <l.audric>
2019-07-09 07:25:37 +0200

    first commit

---
+++ Blog/index.php
@@ -0,0 +1,39 @@
+
+<?php 
+ $validation=$_GET;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+    <link rel="stylesheet" href="css/blog.css">
+
+</head>
+
+
+<body>
+
+<div class="bar_menu isbordered"></div>
+<div class="container isbordered">
+  <div class="bloc_extrait_index isbordered">
+      <div class="bloc_titre isbordered"></div>
+      <div class="sous_bloc_date-text isbordered"></div>
+<div class="bloc_date isbordered"></div> 
+<div class="bloc_text isbordered">
+<div class="bloc_lire-la-suite isbordered"></div></div> 
+
+
+  </div> 
+</div>
+
+
+
+</body>
+
+</html>
