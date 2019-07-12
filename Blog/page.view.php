cc4f901aae997baa91db60e15375e0a1ddd85e27
Audric Larose <l.audric>
2019-07-12 09:50:50 +0200

    finalisation de la section de la zone commentaire + commencement de la partie Admin

--- Blog/page.view.php
+++ Blog/page.view.php
@@ -20,7 +20,14 @@
 
 
 <body>
-     <div class="bloc_header "></div>
+  <?php session_start() ?>
+  <div class="bloc_header ">
+  <div class='bandereau'>
+    <div class='rect_noir'>
+      <div class='nom_artiste'><h2> BLOG DE L'ECRIVAIN</h2></div>
+    </div>
+  </div>
+     </div>
 <div class="bar_menu "></div>
 <div class='big_box'>
    <?php include_once 'side_menu.view.php';?>
@@ -36,34 +43,56 @@ $texto = $show->lecture();
 ?>
 </div>
 <div class='comment'>
-  <div class="show_comment"> Pas de commmentaire , soyez le premier a réagir !
-
-<form action='view.model.php' method="POST">
+  <div class="show_comment">
 
     <?php
 $comment=new affichage;
 $commentaire= $comment->show_comment();
-$ide=$_GET['id'];
+if (isset($_GET['id'])){
+$ide=$_GET['id'];} else { header('location:index.php');}
+if ($commentaire){
 foreach ($commentaire as $commentaires) {
   $nom=$commentaires['auteur'];
   $auteur=$comment->forme($nom);
-    echo '<br> <div class="auteur"><strong>'.$auteur.'</strong></div>';
+  echo "<form action='view.model.php' method='POST'>";
+ echo '<br> <div class="auteur"><strong>'.$auteur.'</strong></div>';
   echo '<div class="date">'.$commentaires['date'].'</div>';
   echo '<div class="conversa">'.$commentaires['commentaire'].'</div>';
- echo '<input name="idk" type="hidden" value="'.$ide.'"/>';
-echo' <button type="submit" name="supprimer">supprimer</button>';
+  if (isset($_SESSION['user'])){
+  if ($commentaires['mail']==$_SESSION['user']){
+    echo' <button type="submit" name="supprimer">supprimer</button>';
+    echo' <button type="submit" name="maj">modifier</button>';
+
+
+  }
+}
+$ide=intval($ide);
+var_dump($ide);
+echo' <button type="submit" name="signaler">signaler</button>';
+ echo '<input name="idk" type="hidden" value="'.$commentaires['id'].'"/>';
+ echo '<input name="idp" type="hidden" value="'.$ide.'"/></form>';
+ 
+
+}} else {
+  echo' Pas de commentaire, soyez le premier a vous exprimer !';
 }
     ?>
   </div>
 
 </div>
-<input type="text" name="nom"></input>
-<input type="text" name="email"></imput>
-<textarea name="commentairer"  cols="100" rows="5" class='comment' placeholder="Tapez votre commentaire"></textarea><br />
-<button type="submit" name="commentaire">commenter</button></form>
+<div class='formulaire'>
+<form action='view.model.php' method='POST'>
+  <input name="idk" type="hidden" value=' <?php echo($_GET["id"])?>'/>
+<input type="text" name="nom" placeholder="Nom"></input><br><br>
+<input type="text" name="email" placeholder="email"></imput><br><br>
+<textarea name="commentairer"  cols="70" rows="5" class='comment' placeholder="Tapez votre commentaire"></textarea><br /><br>
+<?php 
+if (isset($_GET['erreur'])){echo $erreur;} ?>
+<button type="submit" name="commentaire">commenter</button><br>
 </div>
 </div>
 
+</div>
 <div class="bar_footer"></div> 
   </div>
 </body>
--- Blog/view.model.php
+++ Blog/view.model.php
@@ -2,7 +2,7 @@
  include_once 'connexion.model.php';
 class affichage extends Connexion {
 	public function spot (){	
-		$req='SELECT * FROM posts4'  ;
+		$req='SELECT * FROM posts4 ORDER BY id' ;
 		$resultat=$this->connected()->query($req);
 	while ($x=$resultat->fetch()){
 				$data[]=$x;
@@ -21,53 +21,124 @@ $extrait=implode(" ", $extr);
 return $extrait;
 	}
 	public function lecture(){
+		if (isset($_GET['id'])){
 		$id=$_GET['id'];
 		$ide=intval($id);
 		$req='SELECT * FROM posts4 WHERE id='.$ide;
 		$resultat=$this->connected()->query($req);
-		while ($x=$resultat->fetch()){
-				$data[]=$x;
-			}
-		return $data;
-
-	}
-public function show_comment (){
-	$id=$_GET['id'];
-		$ide=intval($id);
-		$req='SELECT * FROM commentaire WHERE id_comment='.$ide;
-		$resultat=$this->connected()->query($req);
+		if($resultat->rowCount()){
 		while ($x=$resultat->fetch()){
 				$data[]=$x;
 			}
 		return $data;
 }
+	} else {	header('location:index.php');
+}}
+public function show_comment (){
+		$id=$_GET['id'];
+		$ide=intval($id);
+		var_dump($ide);
+		$req='SELECT * FROM commentaire WHERE id_comment='.$ide;
+		$resultat=$this->connected()->query($req);
+if($resultat->rowCount()){
+		while ($x=$resultat->fetch()){
+				$data[]=$x;
+			}
+		return $data;
+}}
  public function commentaire (){
  	$commentaire =$_POST['commentairer'];
- 	 	$nom =$_POST['nom'];
+ 	$nom =$_POST['nom'];
  	$mail =$_POST['email'];
  	$id=$_POST['idk'];
+ 	 	$idp=$_POST['idp'];
 	$req="INSERT INTO commentaire (auteur, mail, commentaire, signalement, id_comment) VALUES ('$nom','$mail','$commentaire','0', '$id')"; 
 	echo'ok';
-
 	$resultat=$this->connected()->query($req);
-echo'ok';
-	header('locations:page.view.php');
+	session_start();
+	session_destroy();
+	session_start();
+	$_SESSION['user']=$mail;
+	header('location:page.view.php?id='.$idp);
 
  }
 
+	public function supprimer(){
+$id=$_POST['idk'];
+$ide=intval($id);
+		 	$idp=$_POST['idp'];
+
+$req="DELETE FROM commentaire WHERE id=".$ide;
+$resultat=$this->connected()->query($req);
+	header('location:page.view.php?id='.$idp);
+
+}
+
 public function forme ($x){
 	$texte1=strtolower($x);
-
 	$texte_mod=ucwords($texte1);
-
 	  return $texte_mod;
-}
 
 
 }
+	public function modifier(){
+$id=$_POST['idk'];
+$ide=intval($id);
+var_dump($ide);
+$req="SELECT id , commentaire FROM commentaire WHERE id=".$ide;
+$resultat=$this-> connected()->query($req);
+while ($row=$resultat->fetch()) {
+	$datas[]=$row;
+}
+return $datas;
+}
 
+public function transforme (){
+	$body_modifier=$_POST['body'];
+	$id=$_POST['id'];
+			 	$idp=$_POST['idp'];
+
+	$ide=intval($id);
+	$req="UPDATE commentaire SET commentaire='$body_modifier' WHERE id=".$ide;
+	$resultat=$this-> connected()->query($req);
+	header('location:page.view.php?id='.$idp);
+}
+ public function signale(){
+		$id=$_POST['idk'];
+		$ide=intval($id);
+		 	$idp=$_POST['idp'];
+ 		$req="UPDATE commentaire SET signalement=signalement+1 WHERE id=".$ide;
+		$resultat=$this->connected()->query($req);
+ 	header('location:page.view.php?id='.$idp);
+
+ }
+
+}
 
 if (isset($_POST['commentaire'])){
 	$action =new affichage;
 	$action->commentaire();
 }  
+if (isset($_POST['supprimer'])){
+	$action =new affichage;
+	$action->supprimer();
+}  
+if (isset($_POST['maj'])){ 
+$paragraphe = new affichage;
+$magie=$paragraphe->modifier();
+var_dump($magie);
+$bodys=$magie[0]['commentaire'];
+$id=$magie[0]['id'];
+var_dump($id);
+echo "<form action='view.model.php' method='post'>
+<select name='id' id=''><br /><option name='id' value='$id' >$id</option></select><textarea name='body' cols='30' rows='10'>$bodys</textarea><br /> <button type='submit' name='modifier'>affichageer</button>
+</form>";
+}
+if (isset($_POST['modifier'])){
+	$paragraphe=new affichage;
+	$paragraphe->transforme();
+}
+if (isset($_POST['signaler'])){
+	$paragraphe=new affichage;
+	$paragraphe->signale();
+}
