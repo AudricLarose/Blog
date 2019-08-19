01654d74de38df15c6608386bf9d3759b59f186e
Audric <52261142+AudricLarose@users.noreply.github.com>
2019-08-19 20:00:40 +0200

    Voici la view

---
+++ View/brouillon.admin.view.php
@@ -0,0 +1,22 @@
+    <?php ob_start();?>
+    <br>
+    <h2 class="center">Brouillon</h2>
+    <table class="redimension_table">
+  <tr>
+    <th class="center">titre du brouillon</th>
+ 
+</tr>
+
+
+  <?php 
+ foreach ($brouillon as $brouillons) {
+ 	?><tr><td class="center">
+ <a href="index.php?action=modification&id=<?= $brouillons['id'] ?>"><p><?= $brouillons['title'] ?></p></a>
+<form action='index.php?action=montrer_ecriture' method='POST'>
+<input name="idk" type="hidden" value="<?= $brouillons['id']?>"/>
+<button name='supprimer_brouillon' class ='suppression' value='supprimer'>Supprimer</button></form></td></tr>
+<?php }  ?>
+</table>
+</div>
+	<?php $content_brouillon=ob_get_clean();?>
+
---
+++ View/erreur_404.php
@@ -0,0 +1,5 @@
+<?php ob_start();?>
+<div class='error_404'>
+	<img src="View/404_erreur.png" alt='404_erreur'>
+</div>
+<?php $content=ob_get_clean();?>
