0bde61ff96e556a21a335d296338d34ed2d949ff
Audric <52261142+AudricLarose@users.noreply.github.com>
2019-08-20 11:56:07 +0200

    maj normes

--- Controller/Admin_controller.php
+++ Controller/Admin_controller.php
@@ -1,36 +1,38 @@
 <?php
+namespace controller;
 class Admin_controller
 {
-    public function admin($error){
+    public function admin($error)
+    {
         $content_onglet_titre="Admin connection";
         $session=sessionactive();
         try {
-        if (!isset($session)){
-                if (!isset($error)) {$content_admin= " ";
+            if (!isset($session)){
+                if (!isset($error)){
+                    $content_admin= " ";
+                } else {
+                    switch ($error) {
+                        case 'wrongpwd':
+                            $content_admin= " le mot de passe/login est erroné";
+                        break;
+                        case 'champs_vide':
+                            $content_admin= " il faut remplir les champs avant de valider";
+	                    break;
+		                case ' ':
+                            $content_admin= " ";
+                        break;
+                    }
+                }
+                require 'View/administrateur.view.php';
+                $body= new Body_controller;
+                $body->body($content, $content_onglet_titre);
+            } else {
+                header('location:index.php');
             }
-            else {
-            switch ($error) {
-            case 'wrongpwd':
-                $content_admin= " le mot de passe/login est erroné";
-            break;
-            case 'champs_vide':
-            $content_admin= " il faut remplir les champs avant de valider";
-	        break;
-		    case ' ':
-            $content_admin= " ";
-            break;
+        } catch (Exception $e) {
+            $content = $e->getMessage();
+            $body= new Body_controller;
+            $body->body($content, $content_onglet_titre);
             }
-            }
-    require 'View/administrateur.view.php';			
-    $body= new Body_controller;
-    $body->body($content,$content_onglet_titre);
-    }else {
-    header('location:index.php');
     }
-    } catch (Exception $e){
-    $content = $e->getMessage();
-    $body= new Body_controller;
-    $body->body($content,$content_onglet_titre);
-    }
-    }
-    }
+}
